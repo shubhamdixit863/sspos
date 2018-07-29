afbc2af07465dd9a38ba70e839645d85016437a8
sunnypathak <35329293+sunnypathak@users.noreply.github.com>
2018-07-29 17:05:38 +0530

    ss

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
+
 import {NgForm} from '../../../node_modules/@angular/forms';
-=======
->>>>>>> 77fd9d494d60324e041a232f2ce8074c8f4e62b0
+
 
 @Component({
   selector: 'app-contact',
@@ -15,13 +14,12 @@ export class ContactComponent implements OnInit {
 
   ngOnInit() {
   }
-<<<<<<< HEAD
+
   contactDetails(formObj:NgForm){
      console.log(formObj.value.Name);
      console.log(formObj.value.Email);
      console.log(formObj.value.Message);
   }
-=======
->>>>>>> 77fd9d494d60324e041a232f2ce8074c8f4e62b0
+
 
 }
